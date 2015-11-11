7aac0b52f1e9edec4c3e677028a967f377034f76
Piotr Byszewski <piotr.byszewski@mobica.com>
2015-11-12 00:56:55 +0100

    file structure corrections; working cookbook template

--- cookbook/views.py
+++ cookbook/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-# Create your views here.
+
+def index(request):
+    template = loader.get_template('cookbook.html')
+    context = RequestContext(request, {
+        'version': '10.11.2015',
+    })
+    return HttpResponse(template.render(context))
+
+"""
+def getJson(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question_text for p in latest_question_list])
+    return HttpResponse(output)
+"""
