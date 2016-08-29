d071c035081601b1d0a66bcce6d9cf2e8e9d3c98
Piotr Byszewski <piotr.byszewski@mobica.com>
2016-08-29 02:05:05 +0200

    added admin app

--- aphome/urls.py
+++ aphome/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^cookbook/', include('cookbook.urls'))
 ]
--- cookbook/admin.py
+++ cookbook/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import IngredientUnit
+from .models import IngredientType
+from .models import Ingredient
+from .models import DishIngredient
+from .models import DishPhoto
+from .models import DishCategory
+from .models import Dish
+from .models import Category
 
-# Register your models here.
+admin.site.register(IngredientUnit)
+admin.site.register(IngredientType)
+admin.site.register(Ingredient)
+admin.site.register(DishIngredient)
+admin.site.register(DishPhoto)
+admin.site.register(DishCategory)
+admin.site.register(Dish)
+admin.site.register(Category)
